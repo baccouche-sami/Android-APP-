152bdef4f3d65b7fbd448aa296c6e2d7f8907a24
Sami Baccouche <baccouche@et.intechinfo.fr>
2020-10-04 04:02:29 +0200

    step three

--- app/src/main/java/com/example/android/mumenproject/CommandeActivity.java
+++ app/src/main/java/com/example/android/mumenproject/CommandeActivity.java
@@ -2,13 +2,44 @@ package com.example.android.mumenproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class CommandeActivity extends AppCompatActivity {
-
+    //Spinner paysSpinner;
+    TextView btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_commande);
+
+        btn = findViewById(R.id.suiv);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(view.getContext(), Commande2Activity.class);
+                startActivity(i);
+            }
+        });
+
+        /*
+        paysSpinner = (Spinner) findViewById(R.id.pays);
+
+        // Create an ArrayAdapter using the string array and a default spinner
+        ArrayAdapter<CharSequence> staticAdapter = ArrayAdapter
+                .createFromResource(this, R.array.pays_array,
+                        android.R.layout.simple_spinner_item);
+
+        // Specify the layout to use when the list of choices appears
+        staticAdapter
+                .setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Apply the adapter to the spinner
+        paysSpinner.setAdapter(staticAdapter);*/
     }
 }
